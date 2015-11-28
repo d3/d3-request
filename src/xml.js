fdcb8bc9641739aa79429b101e5a88ae5684d03b
Mike Bostock <mbostock@gmail.com>
2015-11-28 09:04:22 -0800

    Report error if XML is invalid.
    Check if responseXML is undefined. Fixes mbostock/d3#2659.

--- src/xml.js
+++ src/xml.js
@@ -1,5 +1,7 @@
 import requestType from "./requestType";
 
 export default requestType("application/xml", function(xhr) {
-  return xhr.responseXML;
+  var xml = xhr.responseXML;
+  if (!xml) throw new Error("parse error");
+  return xml;
 });

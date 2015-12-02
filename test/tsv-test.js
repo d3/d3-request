b155968e259a83b7fac322c246cf82dac7222d40
Mike Bostock <mbostock@gmail.com>
2015-12-01 16:37:47 -0800

    Fix tests. Fixes #6.

--- test/tsv-test.js
+++ test/tsv-test.js
@@ -1,5 +1,6 @@
 var tape = require("tape"),
-    request = require("../");
+    request = require("../"),
+    table = require("./table");
 
 require("./XMLHttpRequest");
 
@@ -10,7 +11,7 @@ tape("tsv(url, callback) makes an asynchronous GET request for a TSV file", func
     test.equal(XMLHttpRequest._last._info.method, "GET");
     test.equal(XMLHttpRequest._last._info.async, true);
     test.equal(XMLHttpRequest._last._info.mimeType, "text/tab-separated-values");
-    test.deepEqual(data, [{Hello: "42", World: "\"fish\""}]);
+    test.deepEqual(data, table([{Hello: "42", World: "\"fish\""}], ["Hello", "World"]));
     test.end();
   });
 });
@@ -22,7 +23,7 @@ tape("tsv(url, callback) is an alias tsv(url).get(callback)", function(test) {
     test.equal(XMLHttpRequest._last._info.method, "GET");
     test.equal(XMLHttpRequest._last._info.async, true);
     test.equal(XMLHttpRequest._last._info.mimeType, "text/tab-separated-values");
-    test.deepEqual(data, [{Hello: "42", World: "\"fish\""}]);
+    test.deepEqual(data, table([{Hello: "42", World: "\"fish\""}], ["Hello", "World"]));
     test.end();
   });
 });
@@ -30,7 +31,7 @@ tape("tsv(url, callback) is an alias tsv(url).get(callback)", function(test) {
 tape("tsv(url, row, callback) observes the specified row conversion function", function(test) {
   request.tsv("test/data/sample.tsv", function(d) { d.Hello = -d.Hello; return d; }, function(error, data) {
     if (error) throw error;
-    test.deepEqual(data, [{Hello: -42, World: "\"fish\""}]);
+    test.deepEqual(data, table([{Hello: -42, World: "\"fish\""}], ["Hello", "World"]));
     test.end();
   });
 });
@@ -38,7 +39,7 @@ tape("tsv(url, row, callback) observes the specified row conversion function", f
 tape("tsv(url, row, callback) is an alias for tsv(url).row(row).get(callback)", function(test) {
   request.tsv("test/data/sample.tsv").row(function(d) { d.Hello = -d.Hello; return d; }).get(function(error, data) {
     if (error) throw error;
-    test.deepEqual(data, [{Hello: -42, World: "\"fish\""}]);
+    test.deepEqual(data, table([{Hello: -42, World: "\"fish\""}], ["Hello", "World"]));
     test.end();
   });
 });
@@ -46,7 +47,7 @@ tape("tsv(url, row, callback) is an alias for tsv(url).row(row).get(callback)", 
 tape("tsv(url).mimeType(type).get(callback) observes the specified mime type", function(test) {
   request.tsv("test/data/sample.tsv").mimeType("text/plain").get(function(error, data) {
     if (error) throw error;
-    test.deepEqual(data, [{Hello: "42", World: "\"fish\""}]);
+    test.deepEqual(data, table([{Hello: "42", World: "\"fish\""}], ["Hello", "World"]));
     test.equal(XMLHttpRequest._last._info.mimeType, "text/plain");
     test.end();
   });
